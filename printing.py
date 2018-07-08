ba7427631ae995ef2112c3584730de444958380a
Tristan Henning <tristan@customcrypto.com>
2018-07-08 18:43:24 -0500

    reformated documentation to work with help()

--- printing.py
+++ printing.py
@@ -6,8 +6,19 @@ import records as records # generate dayfile name...
 from cart import *
 
 def printReceipt(cart):
+    """
+    printReceipt(cart)
+    usage:
+        creates the formatted receipt and prints it
+        must be called with a cart object from Simple-POS/cart.py
+    """
     return
 
 def printDayFile():
+    """
+    printDayFile()
+    usage:
+        Parse and print the CSV file summary.
+    """
     import records as records
     return
